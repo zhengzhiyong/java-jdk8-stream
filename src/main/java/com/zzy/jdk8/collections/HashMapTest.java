c5278d483780aec016cd6e7fe8504c9160fc40f2
ZZYPC <zhengzhiyong@wxyundian.com>
2020-12-08 15:41:12 +0800

    hashMap学习笔记

--- src/main/java/com/zzy/jdk8/collections/HashMapTest.java
+++ src/main/java/com/zzy/jdk8/collections/HashMapTest.java
@@ -9,13 +9,19 @@ import java.util.stream.LongStream;
 public class HashMapTest {
     @Test
     public void test1(){
+        System.out.println("================start=================");
         Map<Long,String> longStringMap = new HashMap<>();
-        LongStream.rangeClosed(1L,11L)
+        for (int i = 0;i<11;i++){
+            longStringMap.put((long)i,String.valueOf(i));
+        }
+        System.out.println("================end=================");
+        LongStream.rangeClosed(1L,30L)
                 .forEach(l->{
                     longStringMap.put(l,String.valueOf(l));
                 });
-        System.out.println(longStringMap);
         System.out.println((int)Math.pow(2,30) == 1 << 30);
         System.out.println(("hashMap的最大容量："+ (1 << 30)));
+
+        System.out.println((16 << 1) + "     " + ((int)(0.75 * 16) << 1));
     }
 }
